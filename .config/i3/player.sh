b480b2c57caff67ca2ef234e16f3645488665c7a
Ivan Chebykin <ivan@chebykin.org>
2017-07-05 21:18:44 +0300

    Added second prefix to tmux, revent workspaces in i3, improved compton config

--- .config/i3/player.sh
+++ .config/i3/player.sh
@@ -4,7 +4,6 @@ IS_PLAYING="`playerctl status 2>/dev/null`"
 if [ "$IS_PLAYING" = 'Playing' ]; then
 	P_ARTIST=`playerctl metadata xesam:artist`
 	P_TITLE=`playerctl metadata xesam:title`
-	P_ALBUM=`playerctl metadata xesam:album`
-	PLAYER="P: $P_ARTIST - $P_TITLE from $P_ALBUM"
+	PLAYER="♪: $P_ARTIST - $P_TITLE"
 fi
 echo "$PLAYER" | tr -s '{}&' ' ' || exit 1
