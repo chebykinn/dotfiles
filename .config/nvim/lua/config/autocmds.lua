1c8999c2cafb1bf0f012a5972fb138a802606701
Ivan Chebykin <ivan@chebykin.org>
2026-03-26 10:49:36 -0700

    fix italic

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,26 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Remove background/italic from keywords so they don't look highlighted
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    local groups = {
+      "@keyword",
+      "@keyword.modifier",
+      "@keyword.coroutine",
+      "@keyword.return",
+      "@keyword.typescript",
+      "@lsp.type.keyword",
+    }
+    for _, g in ipairs(groups) do
+      local hl = vim.api.nvim_get_hl(0, { name = g, link = false })
+      if next(hl) then
+        hl.bg = nil
+        hl.italic = false
+        vim.api.nvim_set_hl(0, g, hl)
+      end
+    end
+  end,
+})
+vim.cmd("doautocmd ColorScheme")
